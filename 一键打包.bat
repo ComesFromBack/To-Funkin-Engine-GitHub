a3198b9b806d1caeacc98f25ddd7afbbbde41e59
Comes_FromBack <136216585+ComesFromBack@users.noreply.github.com>
2024-10-01 13:55:32 +0800

    Add files via upload

--- 一键打包.bat
+++ 一键打包.bat
@@ -1,7 +1,8 @@
 @echo off
-haxelib run flixel-tools %*
-
-echo This is a quick pack bat file packaging
-pause
+title 少女祈祷中...（Game is Building...）
+cls
+echo 游戏构建中，请耐心等待捏§(*￣▽￣*)§
+echo 要是无聊的话就去听一会音乐吧φ(゜▽゜*)
 lime test windows
+
 pause
